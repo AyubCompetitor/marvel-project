f4f1f307a702ceca22960f042dcb08fdf37b2b36
Ayub <krizz_02@mail.ru>
2022-06-27 08:24:17 +0300

    marvel 1

---
+++ src/services/MarvelService.js
@@ -0,0 +1,28 @@
+
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=718f218b7eb06dec23118d1f5a1fb0f6';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    }
+
+    geCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    }
+}
+
+
+
+export default MarvelService;
